200a2fd26306613125b8a063e5e3800cccb8aa2e
orshemesh <chikos2011@gmail.com>
2020-03-22 16:23:43 +0200

    just 0 and 13 send messege to evreyone :/

--- dsr.cc
+++ dsr.cc
@@ -1,183 +1,156 @@
-#include <iostream>
- #include "ns3/core-module.h"
- #include "ns3/network-module.h"
- #include "ns3/internet-module.h"
- #include "ns3/mobility-module.h"
- #include "ns3/dsr-module.h"
- #include "ns3/applications-module.h"
- #include "ns3/yans-wifi-helper.h"
+#include "ns3/dsr-module.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/applications-module.h"
 #include "ns3/netanim-module.h"
+#include "ns3/flow-monitor-module.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cmath>
 
- using namespace ns3;
- using namespace dsr;
+using namespace ns3;
 
- NS_LOG_COMPONENT_DEFINE ("manet-routing-compare");
+int packetsSent = 0;
+int packetsReceived = 0;
 
- class RoutingExperiment
- {
- public:
-   RoutingExperiment (); //constructor
-   void Run ();
-
- private:
-   Ptr<Socket> SetupPacketReceive (Ipv4Address addr, Ptr<Node> node);
-   void ReceivePacket (Ptr<Socket> socket);
-
-   uint32_t port;
-   uint32_t bytesTotal;
-   uint32_t packetsReceived;
- };
-
- RoutingExperiment::RoutingExperiment ()
-   : port (9),
-     bytesTotal (0),
-     packetsReceived (0)
- {
- }
-
-//  void RoutingExperiment::ReceivePacket (Ptr<Socket> socket)
-//  {
-//    Ptr<Packet> packet;
-//    Address senderAddress;
-//    while ((packet = socket->RecvFrom (senderAddress)))
-//      {
-//        bytesTotal += packet->GetSize ();
-//        packetsReceived += 1;
-//      }
-// std::cout<<bytesTotal;
-//  }
-//
-//
-//  Ptr<Socket> RoutingExperiment::SetupPacketReceive (Ipv4Address addr, Ptr<Node> node)
-//  {
-//    TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
-//
-//    Ptr<Socket> sink = Socket::CreateSocket (node, tid);
-//    InetSocketAddress local = InetSocketAddress (addr, port);
-//    sink->Bind (local);
-//    sink->SetRecvCallback (MakeCallback (&RoutingExperiment::ReceivePacket, this));
-//
-//    return sink;
-//  }
-
-
- void RoutingExperiment::Run ()
- {
-
-   int nWifis = 14;
-
-   double TotalTime = 200.0;
-   std::string rate ("2048bps");
-   std::string phyMode ("DsssRate11Mbps");
-
-
-   Config::SetDefault  ("ns3::OnOffApplication::PacketSize",StringValue ("64"));
-   Config::SetDefault ("ns3::OnOffApplication::DataRate",  StringValue (rate));
-
-
-   NodeContainer nodes;
-   nodes.Create (nWifis);
-
-   // setting up wifi phy and channel using helpers
-   WifiHelper wifi;
-   wifi.SetStandard (WIFI_PHY_STANDARD_80211b);
-
-   YansWifiPhyHelper wifiPhy =  YansWifiPhyHelper::Default ();
-   YansWifiChannelHelper wifiChannel;
-   wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
-   wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel");
-   wifiPhy.SetChannel (wifiChannel.Create ());
-
-   // Add a mac and disable rate control
-   WifiMacHelper wifiMac;
-   wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
-                                 "DataMode",StringValue (phyMode),
-                                 "ControlMode",StringValue (phyMode));
-
-   wifiMac.SetType ("ns3::AdhocWifiMac");
-   NetDeviceContainer adhocDevices = wifi.Install (wifiPhy, wifiMac, nodes);
-
-
-
-
-   MobilityHelper mobilityAdhoc;
-   mobilityAdhoc.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                "MinX", DoubleValue (0.0),
-                                "MinY", DoubleValue (0.0),
-                                "DeltaX", DoubleValue (500),
-                                "DeltaY", DoubleValue (500),
-                                "GridWidth", UintegerValue (5),
-                                "LayoutType", StringValue ("RowFirst"));
-   mobilityAdhoc.SetMobilityModel ("ns3::ConstantVelocityMobilityModel");
-   mobilityAdhoc.Install (nodes);
-int jump=1,jump1=0,jump2=1;
-   for (uint n=0 ; n < nodes.GetN() ; n++)
+void ReceivePacket (Ptr<Socket> socket)
+{
+  Ptr<Packet> packet;
+  while ((packet = socket->Recv ()))
     {
-      if(n<4){
-       Ptr<ConstantVelocityMobilityModel> mob = nodes.Get(n)->GetObject<ConstantVelocityMobilityModel>();
-       mob->SetVelocity(Vector(0, 0, 0));
-       mob->SetPosition(Vector(jump, 1.15, 0));
-       jump++;
+	  packetsReceived++;
+      std::cout<<"Received packet - "<<packetsReceived<<" and Size is "<<packet->GetSize ()<<" Bytes."<<std::endl;
+    }
+}
 
-     }
-     if(n>=4&&n<9){
+static void GenerateTraffic (Ptr<Socket> socket, uint32_t pktSize,
+                             uint32_t pktCount, Time pktInterval )
+{
+  if (pktCount > 0)
+    {
+      socket->Send (Create<Packet> (pktSize));
+      packetsSent++;
+      std::cout<<"Packet sent - "<<packetsSent<<std::endl;
+
+      Simulator::Schedule (pktInterval, &GenerateTraffic,
+                           socket, pktSize,pktCount-1, pktInterval);
+    }
+  else
+    {
+      socket->Close ();
+    }
+}
+
+int main(int argc, char **argv)
+{
+  uint32_t size=14;
+  double step=100;
+  double totalTime=100;
+
+  int packetSize = 1024;
+  int totalPackets = totalTime-1;
+  double interval = 1.0;
+  Time interPacketInterval = Seconds (interval);
+
+  NodeContainer nodes;
+  NetDeviceContainer devices;
+  Ipv4InterfaceContainer interfaces;
+
+  std::cout << "Creating " << (unsigned)size << " nodes " << step << " m apart.\n";
+  nodes.Create (size);
+
+  MobilityHelper mobilityAdhoc;
+  mobilityAdhoc.SetPositionAllocator ("ns3::GridPositionAllocator",
+                               "MinX", DoubleValue (0.0),
+                               "MinY", DoubleValue (0.0),
+                               "DeltaX", DoubleValue (500),
+                               "DeltaY", DoubleValue (500),
+                               "GridWidth", UintegerValue (5),
+                               "LayoutType", StringValue ("RowFirst"));
+  mobilityAdhoc.SetMobilityModel ("ns3::ConstantVelocityMobilityModel");
+  mobilityAdhoc.Install (nodes);
+int jump=1,jump1=0,jump2=1;
+  for (uint n=0 ; n < nodes.GetN() ; n++)
+   {
+     if(n<4){
       Ptr<ConstantVelocityMobilityModel> mob = nodes.Get(n)->GetObject<ConstantVelocityMobilityModel>();
       mob->SetVelocity(Vector(0, 0, 0));
-      mob->SetPosition(Vector(jump1, 2.3, 0));
-         jump1+=2;
+      mob->SetPosition(Vector(jump, 1.15, 0));
+      jump++;
+
     }
-    if(n>=9){
+    if(n>=4&&n<9){
      Ptr<ConstantVelocityMobilityModel> mob = nodes.Get(n)->GetObject<ConstantVelocityMobilityModel>();
      mob->SetVelocity(Vector(0, 0, 0));
-     mob->SetPosition(Vector(jump2, 3.3, 0));
-      jump2++;
+     mob->SetPosition(Vector(jump1, 2.3, 0));
+        jump1+=2;
    }
-    }
+   if(n>=9){
+    Ptr<ConstantVelocityMobilityModel> mob = nodes.Get(n)->GetObject<ConstantVelocityMobilityModel>();
+    mob->SetVelocity(Vector(0, 0, 0));
+    mob->SetPosition(Vector(jump2, 3.3, 0));
+     jump2++;
+  }
+   }
+
+  WifiMacHelper wifiMac ;
+  wifiMac.SetType ("ns3::AdhocWifiMac");
+  YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
+  YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
+  wifiPhy.SetChannel (wifiChannel.Create ());
+  WifiHelper wifi ;
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", "DataMode", StringValue ("OfdmRate6Mbps"), "RtsCtsThreshold", UintegerValue (0));
+  devices = wifi.Install (wifiPhy, wifiMac, nodes);
+
+  DsrMainHelper dsrMain;
+  DsrHelper dsr;
+  InternetStackHelper stack;
+  stack.Install (nodes);
+  dsrMain.Install (dsr, nodes);
+  Ipv4AddressHelper address;
+  address.SetBase ("10.0.0.0", "255.0.0.0");
+  interfaces = address.Assign (devices);
+
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+  Ptr<Socket> recvSink = Socket::CreateSocket (nodes.Get (size-1), tid);
+  InetSocketAddress local = InetSocketAddress (Ipv4Address::GetAny (), 8080);
+  recvSink->Bind (local);
+  recvSink->SetRecvCallback (MakeCallback (&ReceivePacket));
+
+  Ptr<Socket> source = Socket::CreateSocket (nodes.Get (0), tid);
+  InetSocketAddress remote = InetSocketAddress (interfaces.GetAddress (size-1,0), 8080);
+  source->Connect (remote);
+
+  Simulator::Schedule (Seconds (1), &GenerateTraffic, source, packetSize, totalPackets, interPacketInterval);
+
+  std::cout << "Starting simulation for " << totalTime << " s ...\n";
+
+  AnimationInterface anim ("Secure-DSR-in-MANET/dsr-output.xml");
+
+  Ptr<FlowMonitor> flowmon;
+  FlowMonitorHelper flowmonHelper;
+  flowmon = flowmonHelper.InstallAll ();
 
 
-   DsrHelper dsr;
-   DsrMainHelper dsrMain;
-   InternetStackHelper internet;
+  Simulator::Stop (Seconds (totalTime));
+  Simulator::Run ();
+  flowmon->SetAttribute("DelayBinWidth", DoubleValue(0.01));
+  flowmon->SetAttribute("JitterBinWidth", DoubleValue(0.01));
+  flowmon->SetAttribute("PacketSizeBinWidth", DoubleValue(1));
+  flowmon->CheckForLostPackets();
+  flowmon->SerializeToXmlFile("scratch/dsr-flow.xml", true, true);
+  Simulator::Destroy ();
 
-       internet.Install (nodes);
-       dsrMain.Install (dsr, nodes);
+  std::cout<<"\n\n***** OUTPUT *****\n\n";
+  std::cout<<"Total Packets sent = "<<packetsSent<<std::endl;
+  std::cout<<"Total Packets received = "<<packetsReceived<<std::endl;
+  std::cout<<"Packet delivery ratio = "<<(float)(packetsReceived/packetsSent)*100<<" %"<<std::endl;
 
-   NS_LOG_INFO ("assigning ip address");
-
-   Ipv4AddressHelper addressAdhoc;
-   addressAdhoc.SetBase ("10.1.1.0", "255.255.255.0");
-   Ipv4InterfaceContainer adhocInterfaces;
-   adhocInterfaces = addressAdhoc.Assign (adhocDevices);
-
-   OnOffHelper onoff1 ("ns3::UdpSocketFactory",Address ());
-   onoff1.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1.0]"));
-   onoff1.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0.0]"));
-
-//
-//   for (uint i=0 ; i < nodes.GetN() ; i++)
-//       {
-//
-//     //  Ptr<Socket> sink = SetupPacketReceive (adhocInterfaces.GetAddress (i), nodes.Get (i));
-// //  std::cout<<sink;
-//        AddressValue remoteAddress (InetSocketAddress (adhocInterfaces.GetAddress (i), port));
-//        onoff1.SetAttribute ("Remote", remoteAddress);
-//
-//        Ptr<UniformRandomVariable> var = CreateObject<UniformRandomVariable> ();
-//        ApplicationContainer temp = onoff1.Install (nodes.Get (i));
-//        temp.Start (Seconds (var->GetValue (0.0,1.0)));
-//        temp.Stop (Seconds (TotalTime));
-//      }
-
-   Simulator::Stop (Seconds (TotalTime));
-   AnimationInterface anim("anim1.xml");
-   Simulator::Run ();
-   Simulator::Destroy ();
- }
-
-
-
- int main (int argc, char *argv[])
- {
-   RoutingExperiment experiment;
-   experiment.Run ();
- }
+}
