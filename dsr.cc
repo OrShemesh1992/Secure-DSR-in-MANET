e5c8eb5f885f9d10a6683b69bcd377c5f4d8781b
orshemesh <chikos2011@gmail.com>
2020-06-07 13:25:27 +0300

    all the testing in model uses in dsr class

--- dsr.cc
+++ dsr.cc
@@ -27,7 +27,139 @@ static void GenerateTraffic (Ptr<Socket> socket, uint32_t pktSize,
 {
   if (pktCount > 0)
     {
-      socket->Send (Create<Packet> (pktSize));
+      //****************************add option DsrSendBuffer
+
+      dsr::DsrSendBuffer q;
+      q.SetMaxQueueLen (32);
+       q.SetSendBufferTimeout (Seconds (30));
+       Ptr<const Packet> packet = Create<Packet> ();
+       Ipv4Address dst1 = Ipv4Address ("10.1.1.5");
+       dsr::DsrSendBuffEntry e1 (packet, dst1, Seconds (30.2));
+       q.Enqueue (e1);
+       q.Enqueue (e1);
+       q.Enqueue (e1);
+       q.DropPacketWithDst (Ipv4Address ("10.1.1.5"));
+
+      Ipv4Address dst2 = Ipv4Address ("10.1.1.2");
+      dsr::DsrSendBuffEntry e2 (packet, dst2, Seconds (30));
+      q.Enqueue (e1);
+      q.Enqueue (e2);
+      Ptr<Packet> packet2 = Create<Packet> ();
+      dsr::DsrSendBuffEntry e3 (packet2, dst2, Seconds (30));
+      q.Enqueue (e2);
+      q.Enqueue (e3);
+      Ptr<Packet> packet4 = Create<Packet> ();
+      Ipv4Address dst4 = Ipv4Address ("10.1.1.4");
+      dsr::DsrSendBuffEntry e4 (packet4, dst4, Seconds (20));
+      q.Enqueue (e4);
+      q.DropPacketWithDst (Ipv4Address ("10.1.1.4"));
+
+      //CheckSizeLimit ();
+
+      // Simulator::Schedule (q.GetSendBufferTimeout () + Seconds (30), &DsrSendBuffTest::CheckTimeout, this);
+      //
+      // Simulator::Run ();
+      // Simulator::Destroy ();
+
+
+
+
+
+
+
+      //****************************add option rcache
+      Ptr<dsr::DsrRouteCache> rcache = CreateObject<dsr::DsrRouteCache> ();
+      std::vector<Ipv4Address> ip;
+      ip.push_back (Ipv4Address ("10.1.1.1"));
+      ip.push_back (Ipv4Address ("10.1.1.2"));
+      Ipv4Address dst = Ipv4Address ("10.1.1.2");
+      dsr::DsrRouteCacheEntry entry (ip, dst, Seconds (30));
+      // NS_TEST_EXPECT_MSG_EQ (entry.GetVector ().size (), 2, "trivial");
+      // NS_TEST_EXPECT_MSG_EQ (entry.GetDestination (), Ipv4Address ("0.0.0.1"), "trivial");
+      // NS_TEST_EXPECT_MSG_EQ (entry.GetExpireTime (), Seconds (1), "trivial");
+
+      entry.SetExpireTime (Seconds (30));
+      // NS_TEST_EXPECT_MSG_EQ (entry.GetExpireTime (), Seconds (3), "trivial");
+      entry.SetDestination (Ipv4Address ("10.1.1.3"));
+      //NS_TEST_EXPECT_MSG_EQ (entry.GetDestination (), Ipv4Address ("1.1.1.1"), "trivial");
+      ip.push_back (Ipv4Address ("10.1.1.4"));
+      entry.SetVector (ip);
+    //  NS_TEST_EXPECT_MSG_EQ (entry.GetVector ().size (), 3, "trivial");
+      std::cout << " AddRout?  " << rcache->AddRoute (entry)<<"\n";
+      //NS_TEST_EXPECT_MSG_EQ (rcache->AddRoute (entry), true, "trivial");
+
+      // std::vector<Ipv4Address> ip2;
+      // ip2.push_back (Ipv4Address ("1.1.1.0"));
+      // ip2.push_back (Ipv4Address ("1.1.1.1"));
+      // Ipv4Address dst2 = Ipv4Address ("1.1.1.1");
+      // dsr::DsrRouteCacheEntry entry2 (ip2, dst2, Seconds (2));
+      // dsr::DsrRouteCacheEntry newEntry;
+      // NS_TEST_EXPECT_MSG_EQ (rcache->AddRoute (entry2), true, "trivial");
+      // NS_TEST_EXPECT_MSG_EQ (rcache->LookupRoute (dst2, newEntry), true, "trivial");
+      // NS_TEST_EXPECT_MSG_EQ (rcache->DeleteRoute (Ipv4Address ("2.2.2.2")), false, "trivial");
+      //
+      // NS_TEST_EXPECT_MSG_EQ (rcache->DeleteRoute (Ipv4Address ("1.1.1.1")), true, "trivial");
+      // NS_TEST_EXPECT_MSG_EQ (rcache->DeleteRoute (Ipv4Address ("1.1.1.1")), false, "trivial");
+        //****************************add option DsrOptionAckHeader
+      dsr::DsrOptionAckHeader AckHeader;
+
+      AckHeader.SetRealSrc (Ipv4Address ("1.1.1.0"));
+      AckHeader.SetRealDst (Ipv4Address ("1.1.1.1"));
+      AckHeader.SetAckId (1);
+        //****************************add option DsrOptionRerrUnreachHeader
+      dsr::DsrOptionRerrUnreachHeader RerrUnreachHeader;
+      RerrUnreachHeader.SetErrorSrc (Ipv4Address ("1.1.1.0"));
+      RerrUnreachHeader.SetErrorDst (Ipv4Address ("1.1.1.1"));
+      RerrUnreachHeader.SetSalvage (1);
+      RerrUnreachHeader.SetUnreachNode (Ipv4Address ("1.1.1.2"));
+
+      //****************************add option DsrOptionSRHeader
+      dsr::DsrOptionSRHeader sr;
+      std::vector<Ipv4Address> nodeList1;
+      nodeList1.push_back (Ipv4Address ("1.1.1.0"));
+      nodeList1.push_back (Ipv4Address ("1.1.1.1"));
+      nodeList1.push_back (Ipv4Address ("1.1.1.2"));
+      sr.SetNodesAddress (nodeList1);
+      sr.SetSalvage (1);
+      sr.SetSegmentsLeft (2);
+      //****************************add option DsrOptionRrepHeader
+       dsr::DsrOptionRrepHeader h;
+       std::vector<Ipv4Address> nodeList;
+       nodeList.push_back (Ipv4Address ("10.0.0.1"));
+       nodeList.push_back (Ipv4Address ("10.0.0.2"));
+       nodeList.push_back (Ipv4Address ("10.0.0.3"));
+       nodeList.push_back (Ipv4Address ("10.0.0.4"));
+       nodeList.push_back (Ipv4Address ("10.0.0.5"));
+       nodeList.push_back (Ipv4Address ("10.0.0.6"));
+       h.SetNodesAddress(nodeList);
+
+      //****************************add option DsrOptionRreqHeader
+      dsr::DsrOptionRreqHeader rreqHeader;
+      rreqHeader.SetTarget(Ipv4Address ("10.0.0.3"));
+      rreqHeader.SetNumberAddress(10);
+      rreqHeader.SetTarget(Ipv4Address ("10.0.0.9"));
+      rreqHeader.SetId(8);
+      rreqHeader.AddNodeAddress(Ipv4Address ("10.0.0.1"));
+      rreqHeader.AddNodeAddress(Ipv4Address ("10.0.0.2"));
+      rreqHeader.AddNodeAddress(Ipv4Address ("10.0.0.3"));
+      rreqHeader.AddNodeAddress(Ipv4Address ("10.0.0.4"));
+      rreqHeader.SetNodeAddress(1,Ipv4Address ("10.0.0.1"));
+      rreqHeader.SetNodeAddress(2,Ipv4Address ("10.0.0.2"));
+      rreqHeader.SetNodeAddress(3,Ipv4Address ("10.0.0.3"));
+      rreqHeader.SetNodeAddress(4,Ipv4Address ("10.0.0.4"));
+      //****************************install option to header
+      ns3::Packet::EnablePrinting();
+      Ptr<Packet> p = Create<Packet> (pktSize);
+      dsr::DsrRoutingHeader header;
+      header.AddDsrOption (rreqHeader);
+      //****************************add heder to packet
+      p->AddHeader (header);
+      p->RemoveAtStart (8);
+      // p->AddHeader (header);
+      // p->RemoveAtStart (8);
+      std::cout << p->ToString();
+      socket->Send (p);
+
       Simulator::Schedule (pktInterval, &GenerateTraffic,
                            socket, pktSize,pktCount - 1, pktInterval);
     }
@@ -42,7 +174,7 @@ int main (int argc, char *argv[])
 {
   std::string phyMode ("DsssRate1Mbps");
   double distance = 500;  // m
-  uint32_t packetSize = 1000; // bytes
+  uint32_t packetSize = 1024; // bytes
   uint32_t numPackets = 1;
   uint32_t numNodes = 15;  // by default, 5x5
   uint32_t sinkNode = 5;
@@ -96,16 +228,49 @@ int main (int argc, char *argv[])
   //***************************END**************************
 
 //***************************SetPosition*************************
-  MobilityHelper mobilityAdhoc;
-  mobilityAdhoc.SetPositionAllocator ("ns3::GridPositionAllocator",
-                               "MinX", DoubleValue (0.0),
-                               "MinY", DoubleValue (0.0),
-                               "DeltaX", DoubleValue (distance),
-                               "DeltaY", DoubleValue (distance),
-                               "GridWidth", UintegerValue (5),
-                               "LayoutType", StringValue ("RowFirst"));
-  mobilityAdhoc.SetMobilityModel ("ns3::ConstantVelocityMobilityModel");
-  mobilityAdhoc.Install (c);
+MobilityHelper mobilityAdhoc;
+mobilityAdhoc.SetPositionAllocator ("ns3::GridPositionAllocator",
+                             "MinX", DoubleValue (0.0),
+                             "MinY", DoubleValue (0.0),
+                             "DeltaX", DoubleValue (distance),
+                             "DeltaY", DoubleValue (distance),
+                             "GridWidth", UintegerValue (5),
+                             "LayoutType", StringValue ("RowFirst"));
+mobilityAdhoc.SetMobilityModel ("ns3::ConstantVelocityMobilityModel");
+mobilityAdhoc.Install (c);
+int jump=1,jump1=0,jump2=1;
+for (uint n=0 ; n < c.GetN() ; n++)
+ {
+   if(n<4){
+    Ptr<ConstantVelocityMobilityModel> mob = c.Get(n)->GetObject<ConstantVelocityMobilityModel>();
+    mob->SetVelocity(Vector(0, 0, 0));
+    mob->SetPosition(Vector(jump, 1.15, 0));
+    jump++;
+
+  }
+  if(n>=4&&n<9){
+   Ptr<ConstantVelocityMobilityModel> mob = c.Get(n)->GetObject<ConstantVelocityMobilityModel>();
+   mob->SetVelocity(Vector(0, 0, 0));
+   mob->SetPosition(Vector(jump1, 2.3, 0));
+      jump1+=2;
+ }
+ if(n>=9){
+  Ptr<ConstantVelocityMobilityModel> mob = c.Get(n)->GetObject<ConstantVelocityMobilityModel>();
+  mob->SetVelocity(Vector(0, 0, 0));
+  mob->SetPosition(Vector(jump2, 3.3, 0));
+   jump2++;
+}
+ }
+  // MobilityHelper mobilityAdhoc;
+  // mobilityAdhoc.SetPositionAllocator ("ns3::GridPositionAllocator",
+  //                              "MinX", DoubleValue (0.0),
+  //                              "MinY", DoubleValue (0.0),
+  //                              "DeltaX", DoubleValue (distance),
+  //                              "DeltaY", DoubleValue (distance),
+  //                              "GridWidth", UintegerValue (5),
+  //                              "LayoutType", StringValue ("RowFirst"));
+  // mobilityAdhoc.SetMobilityModel ("ns3::ConstantVelocityMobilityModel");
+  // mobilityAdhoc.Install (c);
   //***************************END********************************
 
 //***************************DSR********************************
@@ -135,6 +300,7 @@ for (size_t i = 1; i < c.GetN(); i++) {
      Ptr<Icmpv4L4Protocol> icmp = c.Get (i)->GetObject<Icmpv4L4Protocol> ();
      icmp->SetDownTarget (MakeCallback (&dsr::DsrRouting::Send, agent));
      c.Get (i)->AggregateObject (agent);
+     //agent->SetRoute (Ipv4Address ("10.0.0.7"),Ipv4Address ("10.0.0.9"));
      agent->SetNode (c.Get (i));
 }
 ObjectFactory m_agentFactory;
@@ -151,6 +317,21 @@ ObjectFactory m_agentFactory;
    c.Get (0)->AggregateObject (agent);
 
    Ptr<ns3::dsr::DsrRouteCache> routeCache= CreateObject<ns3::dsr::DsrRouteCache> ();
+
+
+   // dsr::DsrOptionRrepHeader h;
+   //
+   // std::vector<Ipv4Address> nodeList;
+   // nodeList.push_back (Ipv4Address ("1.1.1.0"));
+   // nodeList.push_back (Ipv4Address ("1.1.1.1"));
+   // nodeList.push_back (Ipv4Address ("1.1.1.2"));
+   // h.SetNodesAddress (nodeList);
+//   NS_TEST_EXPECT_MSG_EQ (h.GetNodeAddress (0), Ipv4Address ("1.1.1.0"), "trivial");
+//   NS_TEST_EXPECT_MSG_EQ (h.GetNodeAddress (1), Ipv4Address ("1.1.1.1"), "trivial");
+//   NS_TEST_EXPECT_MSG_EQ (h.GetNodeAddress (2), Ipv4Address ("1.1.1.2"), "trivial");
+
+
+   //uint32_t bytes = p->RemoveHeader (h2);
   // std::vector<Ipv4Address> m_finalRoute;
    // m_finalRoute.push_back(i.GetAddress (4));
    // m_finalRoute.push_back(i.GetAddress (5));
@@ -169,7 +350,7 @@ ObjectFactory m_agentFactory;
 //DsrRouteCacheEntry::DsrRouteCacheEntry (IP_VECTOR const  & ip, Ipv4Address dst, Time exp)
 
 //bool flag1 =routeCache->AddRoute(toPrev);
-agent->SetRouteCache (routeCache);
+//agent->SetRoute (Ipv4Address ("10.0.0.7"),Ipv4Address ("10.0.0.9"));
 bool flag=routeCache->UpdateRouteEntry(i.GetAddress (5));
 std::cout << "success: "<<std::boolalpha<< flag << " " << '\n';
 
